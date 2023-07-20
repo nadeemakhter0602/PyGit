98bbd62d81c790a7d2b2218040eb486c09eaa179
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-07-20 21:56:36 +0530

    parse read creation time from index, add comments

--- index.py
+++ index.py
@@ -7,9 +7,19 @@ def read_index():
         return []
     with open(index_file, "rb") as f:
         raw = f.read()
+    # parse index header
     header = raw[:12]
     signature = raw[:4]
     assert signature == b"DIRC"
     version = int.from_bytes(header[4:8], "big")
     assert version == 2
     count = int.from_bytes(header[8:12], "big")
+    # get index entries
+    entries = []
+    content = raw[12:]
+    idx = 0
+    for i in range(count):
+        # read creation time as a unix timestamp
+        ctime_s = int.from_bytes(content[idx : idx + 4], "big")
+        # read creation time nanoseconds
+        ctime_ns = int.from_bytes(content[idx + 4 : idx + 8], "big")
