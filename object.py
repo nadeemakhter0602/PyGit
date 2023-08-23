dc3732da3db8c324cdbe04c4a047df31a0695fdc
nadeemakhter0602 <65607791+nadeemakhter0602@users.noreply.github.com>
2023-08-23 23:47:17 +0530

    add comments

--- object.py
+++ object.py
@@ -6,12 +6,14 @@ import os
 def hash_object(data, object_type, write=True):
     header = "{} {}".format(object_type, len(data)).encode()
     object_data = header + b"\x00" + data
+    # get SHA-1 hash of object data
     object_hash = hashlib.sha1(object_data, usedforsecurity=False).hexdigest()
     subdir = object_hash[:2]
     object_file = object_hash[2:]
     if write:
         object_path = os.path.join(".git", "objects", subdir, object_file)
         with open(object_path, "wb") as f:
+            # compress object data using zlib
             f.write(zlib.compress(object_data))
     return object_hash
 
